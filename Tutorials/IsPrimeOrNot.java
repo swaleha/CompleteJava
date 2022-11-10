d141f1516705ab988b5c864ed0f42379913878d7
ssp <swalehapeerzade@gmail.com>
2022-11-10 18:36:08 -0500

    commit

---
+++ Tutorials/IsPrimeOrNot.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class IsPrimeOrNot {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        boolean isPrime = true;
+
+        if(number == 2){
+            System.out.println("2 is prime");
+        } else {
+            for (int i = 2; i <= number - 1; i++){
+                if(number % i == 0) {
+                    isPrime = false;
+                }
+            }
+    
+            if(isPrime == true){
+                System.out.println("Number is prime");
+            } else {
+                System.out.println("Number is not prime");
+            }
+        }
+   }
+}
---
+++ Tutorials/IsPrimeOrNotOptimized.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class IsPrimeOrNotOptimized {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
+        boolean isPrime = true;
+        if(number == 2){
+            System.out.println("2 is prime");
+        } else {
+            for(int i = 2; i <= Math.sqrt(number); i++){
+                if((number % i) == 0){
+                    isPrime = false;
+                }
+            }
+
+            if(isPrime == true){
+                System.out.println("Number is prime");
+            } else {
+                System.out.println("Number is not prime");
+            }
+
+        }
+    }
+}
