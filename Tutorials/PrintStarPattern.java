6ca33eca1800e251c24202a937de360a633eefe6
ssp <swalehapeerzade@gmail.com>
2022-11-11 23:29:54 -0500

    commit

---
+++ Tutorials/PrintStarPattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class PrintStarPattern {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        for(int line = 1; line <= n; line++) {
+            for(int star = 1; star <= line; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
