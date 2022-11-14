18db3ce8387d5b3b16cf34fa840a31bbbf919e39
ssp <swalehapeerzade@gmail.com>
2022-11-14 00:09:42 -0500

    commit

---
+++ Tutorials/OverloadingUsingDatatypes.java
@@ -0,0 +1,18 @@
+public class OverloadingUsingDatatypes {
+
+    //func to calc int sum
+
+    public static int sum(int a, int b){
+        return a + b;
+    }
+
+    //func to calc float sum
+
+    public static float sum(float a, float b){
+        return a + b;
+    }
+    public static void main(String args[]){
+        System.out.println(sum(3, 5));
+        System.out.println(sum(3.2f, 4.8f));
+    }
+}
