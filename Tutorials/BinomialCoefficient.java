6197977e702bf007ac419fa05df7372479a88d94
ssp <swalehapeerzade@gmail.com>
2022-11-13 23:38:49 -0500

    commit

---
+++ Tutorials/BinomialCoefficient.java
@@ -0,0 +1,22 @@
+public class BinomialCoefficient {
+    public static int factorial(int num) {
+        int fact = 1;
+        for(int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static int binCoeff(int n, int r){
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n-r);
+
+        int binCoeff = fact_n / (fact_r * fact_nmr);
+        return binCoeff;
+    }
+
+    public static void main(String args[]){
+        System.out.println(binCoeff(5, 2));
+    }
+}
