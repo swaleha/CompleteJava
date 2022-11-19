7535893368194d562e06d804e79f02680ed3ac07
ssp <swalehapeerzade@gmail.com>
2022-11-19 15:08:00 -0500

    commit

---
+++ Tutorials/BinarySearch.java
@@ -0,0 +1,30 @@
+public class BinarySearch {
+
+    public static int binary_search(int numbers[], int key){
+        int start=0, end=numbers.length-1;
+        
+
+        while(start<=end){
+            int mid = (start + end)/2;
+            if(numbers[mid]==key){ //found
+                return mid;       
+            } 
+            if(numbers[mid] < key){ //right
+                start = mid + 1;
+            }
+
+            if(numbers[mid] > key){ //left
+                end = mid - 1;
+            }
+    }
+
+    return -1;
+
+}
+    public static void main(String args[]){
+        int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
+        int key = 8;
+    
+        System.out.println("Key is present at index: " + binary_search(numbers, key));
+   } 
+}
