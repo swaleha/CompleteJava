75a70a917920b2db96b1c1edcb5ad04778f64842
ssp <swalehapeerzade@gmail.com>
2022-11-27 17:44:57 -0500

    commit

--- Assignments/Assignment6a.java
+++ Assignments/Assignment6a.java
@@ -1,7 +1,7 @@
 /*
  * Given an integer array nums, return true if any value appears at least twice in the array, 
  * and return false if every element is distinct.
- * 
+ * Time complexity - O(n2)
  */
 
 public class Assignment6a {
