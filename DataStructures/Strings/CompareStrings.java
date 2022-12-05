e72d0f71cf75aa32b9bcc0e85b01feb176d79754
ssp <swalehapeerzade@gmail.com>
2022-12-04 19:54:44 -0500

    commit

--- DataStructures/Strings/CompareStrings.java
+++ DataStructures/Strings/CompareStrings.java
@@ -1,3 +1,26 @@
 public class CompareStrings {
-    
+    public static void main(String args[]){
+        String s1 = "Arham";
+        String s2 = "Arham";
+        String s3 = new String("Arham");
+
+        // if(s1 == s2){
+        //     System.out.println("Strings are equal");
+        // } else {
+        //     System.out.println("Strings are not equal");
+        // }
+
+        // if(s1 == s3){
+        //     System.out.println("Strings are equal");
+        // } else {
+        //     System.out.println("Strings are not equal");
+        // }
+
+        if(s1.equals(s3)){
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+
+    }
 }
