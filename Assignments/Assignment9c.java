dc602a892732b6a99872fba7057c034dcc48ba34
ssp <swalehapeerzade@gmail.com>
2022-12-06 17:34:00 -0500

    commit

---
+++ Assignments/Assignment9c.java
@@ -0,0 +1,9 @@
+/*
+ * Output of the following code
+ */
+public class Assignment9c {
+    public static void main(String args[]){
+        String str = "HelloWorld".replace("W", " w");
+        System.out.println(str);
+    }
+}
