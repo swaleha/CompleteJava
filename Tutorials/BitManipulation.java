e93b79fa8e2d23fedee59daca4c32d029de7190f
ssp <swalehapeerzade@gmail.com>
2022-12-07 15:52:57 -0500

    commit

---
+++ Tutorials/BitManipulation.java
@@ -0,0 +1,23 @@
+public class BitManipulation {
+    public static void main(String[] args) {
+
+        //Bitwise AND &
+        System.out.println(5&6);
+
+        //Bitwise OR |
+        System.out.println(5|6);
+
+        //Bitwise XOR ^
+        System.out.println(5^6);
+
+        //Binary one's complement (~)
+        System.out.println(~0);
+
+        //Binary left shift (a<<b results in a * pow(2, b)))
+        System.out.println(5<<2);
+
+        //Binary right shift (a>>b results in a / pow(2, b))
+        System.out.println(6>>1);
+        
+    }
+}
