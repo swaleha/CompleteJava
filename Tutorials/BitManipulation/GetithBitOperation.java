d3ddbf4c86a235bc10e621ef09aa03608717efe9
ssp <swalehapeerzade@gmail.com>
2022-12-08 17:19:29 -0500

    commit

---
+++ Tutorials/BitManipulation/GetithBitOperation.java
@@ -0,0 +1,15 @@
+public class GetithBitOperation {
+
+    public static int getIthBit(int n, int i){
+        int bitMask = 1 << i; //keeps the ith bit as 1 and makes remaining bits as 0
+        if((n & bitMask) == 0){ // ith bit is 0
+            return 0;
+        } else {              //ith bit is 1
+            return 1;
+        }
+    }
+    public static void main(String args[]){
+        int n = 10;
+        System.out.println(getIthBit(n, 3)); //2 
+    }
+}
