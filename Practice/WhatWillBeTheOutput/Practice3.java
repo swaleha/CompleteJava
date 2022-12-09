fb7fca2a31352c41aef3b1dcbb959a294af3696d
ssp <swalehapeerzade@gmail.com>
2022-12-08 23:17:08 -0500

    commit

--- Practice/WhatWillBeTheOutput/Practice3.java
+++ Practice/WhatWillBeTheOutput/Practice3.java
@@ -8,7 +8,7 @@
  */
 public class Practice3 {
     public static void main(String[] args) {
-        for(int i=1; i <=5; i++){
+        for(int i=1; i<=5; i++){
             for(int j=i; j>0; j--){
                 System.out.print(i);
             }
