cabbd7381727e6d9b4fad689f86edbc0cfb45264
ssp <swalehapeerzade@gmail.com>
2022-12-11 15:39:49 -0500

    commit

--- Tutorials/BitManipulation/UpdateIthBitOperation.java
+++ Tutorials/BitManipulation/UpdateIthBitOperation.java
@@ -1,16 +1,17 @@
 public class UpdateIthBitOperation{
 
-    public static int clearIthBit(int n, i){
-        int bitMask = 1 << i;
-        
+    public static int clearIthBit(int n, int i){
+        int bitMask = ~(1 << i);
+        return n & bitMask;
     }
 
     public static int updateIthBit(int n, int i){
-        int n = clearIthBit(n, i);    
+        n = clearIthBit(n, i);    
         int bitMask = n << i;
         return n | bitMask;
     }
     public static void main(String[] args) {
-        
+        int n = 10;
+        System.out.println(updateIthBit(10, 1));
     }
 }
