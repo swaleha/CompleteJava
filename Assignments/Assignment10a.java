341c1269ec242d5d5553f7e105a24edfcaf35e89
ssp <swalehapeerzade@gmail.com>
2022-12-11 22:41:42 -0500

    commit

---
+++ Assignments/Assignment10a.java
@@ -0,0 +1,12 @@
+/*
+ * Value of x^x for any value of x
+ */
+public class Assignment10a {
+
+    public static int XOR(int x){
+        return x^x;
+    }
+    public static void main(String[] args) {
+        System.out.println(XOR(5));
+    }
+}
---
+++ Assignments/Assignment10b.java
@@ -0,0 +1,12 @@
+public class Assignment10b {
+    public static void main(String[] args) {
+        int x=3, y=4;
+        System.out.println("Before swap: x = " + x + " y = " + y);
+        
+        //swap using xor
+        x = x ^ y;
+        y = x ^ y;
+        x = x ^ y;
+        System.out.println("After swap: x = " + x + " y = " + y);
+    }
+}
