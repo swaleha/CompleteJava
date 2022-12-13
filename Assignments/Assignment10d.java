101fe96a9df1125a2ea56bb4b1e0eebb0ec0010d
ssp <swalehapeerzade@gmail.com>
2022-12-13 18:39:21 -0500

    commit

---
+++ Assignments/Assignment10d.java
@@ -0,0 +1,13 @@
+/*
+ * Convert uppercase letters to lower case
+ */
+public class Assignment10d {
+    public static void main(String[] args) {
+        //convert uppercase characters to lowercase
+        for(char ch='A'; ch <='Z'; ch++){
+            System.out.print((char) (ch | ' '));
+            //prints abcdefghijklmnopqrstuvwxyz
+        }
+        System.out.println();
+    }
+}
