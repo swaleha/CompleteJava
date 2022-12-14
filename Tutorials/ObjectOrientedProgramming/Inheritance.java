f03da0d733fe105da21b3e90791102c9830951b4
ssp <swalehapeerzade@gmail.com>
2022-12-14 17:34:34 -0500

    commit

--- Tutorials/ObjectOrientedProgramming/Inheritance.java
+++ Tutorials/ObjectOrientedProgramming/Inheritance.java
@@ -1,6 +1,16 @@
 /*
  * Inheritance: One of the pillars of OOPS. Here derived/child class inherits the properties and methods of the base/parent class
- */
+ * 1. Single inheritance: Parent/Base class -> Derived/class
+ * 2. Multi-level inheritance:
+ *  Base class -> derived class1 -> derived class2
+ *  (eg, Animal -> Mammal -> Cat)
+ *  3. Hierarchial inheritance:
+ *  Base class -> Derived class1,
+ *  Base class -> Derived class2,
+ *  Base class -> Derived class3
+ *  Eg, Animal -> Mammal, Animal -> Fish, Animal -> Bird
+ *  4. Hybrid Inheritance: It's the combination of multi-level and hierarchial inheritance
+*/
 
 public class Inheritance{
         public static void main(String[] args) {
@@ -15,10 +25,12 @@ public class Inheritance{
             Bird budgie = new Bird();
             budgie.fly();
             budgie.breathe();
+
+            Mimic parakeet = new Mimic();
+            parakeet.mimicking();
         }
     }
 
-//Base class or parent class
 class Animal {
 
     String color;
@@ -41,17 +53,34 @@ class Fish extends Animal{
     }
 }
 
+class Jawless extends Fish {
+    String mouth = "round";
+}
+
+class Bony extends Fish {
+    String bone = "skeleton";
+}
+
+
 /*
- *  Hierarchial inheritance:
- *  Base class -> Derived class1,
- *  Base class -> Derived class2,
- *  Base class -> Derived class3
- *  Eg, Animal -> Mammal, Animal -> Fish, Animal -> Bird
+ * Hierarchial inheritance
  */
 
 class Bird extends Animal {
     void fly(){
         System.out.println("Flies");
+    }
+}
+
+class Mimic extends Bird {
+    void mimicking(){
+        System.out.println("Mimics");
+    }
+}
+
+class Ratites extends Bird {
+    void run(){
+        System.out.println("Runs");
     }
 }
 
@@ -69,6 +98,18 @@ class Mammal extends Animal {
 
 class Cat extends Mammal {
     String breed;
+    void meow(){
+        System.out.println("Meows");
+    }
+}
+
+
+/* */
+class Dog extends Mammal {
+    String breed;
+    void bark(){
+        System.out.println("barks");
+    }
 }
 
 
