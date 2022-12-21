b8ffcca5120b9dd9aa8d00c016ceafcd23ef739e
ssp <swalehapeerzade@gmail.com>
2022-12-21 17:53:55 -0500

    commit

---
+++ Recursion/SumOfNNumbers.java
@@ -0,0 +1,23 @@
+/*
+ * 
+ */
+public class SumOfNNumbers {
+    public static void main(String[] args) {
+        System.out.println(calculateSum(5));
+    }
+
+    public static int calculateSum(int n) {
+        if(n <= 0){
+            System.out.println("Enter a valid number");
+            return -1;
+        }
+        
+        if (n == 1) {
+            return 1;
+        }
+
+        int snm1 = calculateSum(n-1);
+        int sn = n + snm1;
+        return sn;
+    }
+}
