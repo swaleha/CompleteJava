2bb75669856de6f2c8551f064bdb733cebc6bf82
ssp <swalehapeerzade@gmail.com>
2022-12-21 18:20:56 -0500

    commit

---
+++ Recursion/FibonacciNumber.java
@@ -0,0 +1,18 @@
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("Fibonacci number " + n + " is : " + fib(n));
+    }
+    public static int fib(int n){
+        //Base case
+        if(n == 0 || n == 1){
+            return n;
+        }
+
+        int fibnm1 = fib(n-1);
+        int fibnm2 = fib(n-2);
+        int fibn = fibnm1 + fibnm2;
+        return fibn;
+    }
+
+}
