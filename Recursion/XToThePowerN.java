f33155de919d13da1819a22fd59b4c8fc5f1f9cb
ssp <swalehapeerzade@gmail.com>
2022-12-22 21:22:26 -0500

    commit

---
+++ Recursion/XToThePowerN.java
@@ -0,0 +1,17 @@
+public class XToThePowerN {
+    public static void main(String[] args) {
+        System.out.println(power(2, 10));
+    }
+    public static int power(int x, int n){
+        //Base case
+        if(n == 0){
+            return 1;
+        }
+
+        // int xnm1 = power(x, n-1);
+        // int xn = x * power(x, n-1);
+        // return xn;
+
+        return x * power(x, n-1);
+    }
+}
