7d762dafe9a4023bbcd39e3b059a24773cd94e3d
ssp <swalehapeerzade@gmail.com>
2022-12-22 22:59:50 -0500

    commit

--- Recursion/XToThePowerNOptimized.java
+++ Recursion/XToThePowerNOptimized.java
@@ -1,12 +1,24 @@
 /*
- * 
+ * Time complexity: O(logn)
  */
 public class XToThePowerNOptimized {
     public static void main(String[] args) {
-        System.out.println(power(2, 10));
+        System.out.println(optimizedPower(2, 10));
     }
 
-    public static int power(int x, int n) {
+    public static int optimizedPower(int x, int n) {
+        if(n == 0) {
+            return 1;
+        }
 
+        //n is even
+        int halfPower = optimizedPower(x, n/2);
+        int halfPowerSq = halfPower * halfPower;
+
+        //n is odd
+        if(n%2 != 0){
+            halfPowerSq = x * halfPowerSq;
+        }
+        return halfPowerSq;
     }
 }
