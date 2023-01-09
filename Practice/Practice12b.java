36e8adf468647b79948fa6529f5f3c644961629e
ssp <swalehapeerzade@gmail.com>
2023-01-09 16:48:48 -0500

    commit

---
+++ Practice/Practice12b.java
@@ -0,0 +1,23 @@
+/*
+ * Solved using recursion
+ * Convert a given number (2019) to string (two zero one nine)
+ * digits(0-9)
+ * last digit cannot be 0
+ */
+public class Practice12b {
+    static String digits[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    public static void printDigits(int number) {
+        //Base case
+        if(number == 0) {
+            return;
+        }
+
+        int lastDigit = number % 10;
+        printDigits(number/10);
+        System.out.print(digits[lastDigit] + " ");
+    }
+    public static void main(String[] args) {
+        printDigits(1947);
+        System.out.println();
+    }
+}
