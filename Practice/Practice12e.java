11886d4b6d3ddae7cd775d6de0f3e2a53edcaa7e
ssp <swalehapeerzade@gmail.com>
2023-01-10 17:07:00 -0500

    commit

---
+++ Practice/Practice12e.java
@@ -0,0 +1,30 @@
+/*
+ * Rules:
+ * 1. Only one disk can be moved at a time.
+ * 2. Top-most disk can only be moved
+ * 3. Larger disk cannot be placed on top of a smaller disk
+ */
+
+public class Practice12e {
+    /*
+     * n - number of disks
+     * src - source rod
+     * dest - destination rod
+     * helper - auxillary/helper rod
+     */
+    public static void towerOfHanoi(int n, String src, String helper, String dest) {
+        //Base case
+        if(n == 1) {
+            System.out.println("transfer disk " + n + " from source " + src + " to destination " + dest);
+            return;
+        }
+
+        //work
+        towerOfHanoi(n - 1, src, dest, helper);
+        System.out.println("transfer disk " + n + " from source " + src + " to destination " + dest);
+        towerOfHanoi(n - 1, helper, src, dest);
+    }
+    public static void main(String[] args) {
+        towerOfHanoi(3, "S", "H", "D");
+    }
+}
